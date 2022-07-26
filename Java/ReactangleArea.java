553d03a6a1673d4067dfb684a5c458c733cb5e99
digambar4u <digambarchaudhari425303@gmail.com>
2022-07-26 14:32:34 +0530

    area of rectangle and perimeter program

---
+++ Java/ReactangleArea.java
@@ -0,0 +1,16 @@
+public class ReactangleArea {
+
+    public static void main(String[] args) {
+        int length = 5;
+        int breadth = 7;
+
+        // Formula to calculate area of reactangle
+        int area = length * breadth;
+
+        // Formula to Perimeter area of reactangle
+        int perimeter = (2 * length) + (2 * breadth);
+
+        System.out.println("Area of reactangle is: " + area);
+        System.out.println("Perimeter of reactangle is: " + perimeter);
+    }
+}
